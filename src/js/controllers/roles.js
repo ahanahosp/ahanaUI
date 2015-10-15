9a4f9d45607ccb29c4d82e98a1708413f45478d0
Hari <hari@arkinfotec.com>
2015-10-16 01:08:16 +0530

    Initial Commit

--- src/js/controllers/roles.js
+++ src/js/controllers/roles.js
@@ -5,24 +5,61 @@
 /* Controllers */
 app
 // Registration controller
-    .controller('RolesController', ['$scope', '$http', function($scope, $http) {
-      $scope.saveRoles = function() {
-        console.log($scope.roleForm.$valid);
-        if ($scope.roleForm.$valid) {
-          $http({
-            url: path + "rest/secure/user/createRole",
-            method: "POST",
-            data: $scope.data
-          }).then(
-              function() {
-                $scope.data = {};
-                $scope.message = "Roles saved successfully";
-              },
-              /**Error handling*/
-              function() {
-                $scope.error = "Failed save";
-              }
-          )
-        }
+  .controller('RolesController', ['$scope', '$http','NgTableParams','$state', function($scope, $http, NgTableParams, $state) {
+    $scope.saveRoles = function() {
+      if ($scope.roleForm.$valid) {
+        $http({
+          url: path + "rest/secure/user/createRole",
+          method: "POST",
+          data: $scope.data
+        }).then(
+          function() {
+            $scope.data = {};
+            $scope.message = "Roles saved successfully";
+          },
+          /**Error handling*/
+          function() {
+            $scope.error = "Failed save";
+          }
+        )
       }
-    }]);
+    }
+
+    $scope.loadRolesList = function() {
+      $http({
+        url: path + "rest/secure/user/getActiveRoles",
+        method: "GET"
+      }).then(
+        function(response){
+          var data = response.data.roleDetails;
+          $scope.tableParams = new NgTableParams({ count: 5}, { counts: [5, 10, 25], dataset: data});
+        }
+      )
+    }
+
+    $scope.loadRoleDetails = function(){
+      $http({
+        url: path + "rest/secure/user/getRoleByOid?oid="+$state.params.oid,
+        method: "GET"
+      }).then(
+        function(response){
+          $scope.data = response.data.roles;
+        }
+      )
+    }
+ }]);
+
+(function() {
+  "use strict";
+
+  app.factory("RolesServices", ["$resource", function($resource) {
+    return $resource("https://api.github.com/repos/:username/:repo/issues", {
+      state: "open"
+    }, {
+      query: {
+        method: "GET",
+        isArray: true
+      }
+    });
+  }]);
+})();
