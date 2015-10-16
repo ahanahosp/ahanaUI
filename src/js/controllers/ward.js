113f80d6ac0618b06d066536ea28044556265cd5
Selvaraj K <ahanahosp@gmail.com>
2015-10-16 15:45:00 +0530

    Ward Changes

---
+++ src/js/controllers/ward.js
@@ -0,0 +1,71 @@
+'use strict';
+
+//TODO need to move in properties file
+
+/* Controllers */
+app
+// Registration controller
+  .controller('FloorController', ['$scope', '$http','NgTableParams','$state', function($scope, $http, NgTableParams, $state) {
+	  $http.get(path + "rest/secure/common/getFloorValues").then(
+          function(response) {
+            $scope.floors = response.data.floorDetails;
+          }
+      )
+	      
+    $scope.saveFloor = function() {
+      if ($scope.wardForm.$valid) {
+        $http({
+          url: path + "rest/secure/common/createWard",
+          method: "POST",
+          data: $scope.data
+        }).then(
+          function() {
+            $scope.data = {};
+            $scope.message = "Ward saved successfully";
+          },
+          /**Error handling*/
+          function() {
+            $scope.error = "Failed save";
+          }
+        )
+      }
+    }
+
+    $scope.loadWardList = function() {
+      $http({
+        url: path + "rest/secure/common/getAllWards",
+        method: "GET"
+      }).then(
+        function(response){
+          var data = response.data.wardDetails;
+          $scope.tableParams = new NgTableParams({ count: 5}, { counts: [5, 10, 25], dataset: data});
+        }
+      )
+    }
+
+    $scope.loadWardDetails = function(){
+      $http({
+        url: path + "rest/secure/common/getWardByOid?oid="+$state.params.oid,
+        method: "GET"
+      }).then(
+        function(response){
+          $scope.data = response.data.ward;
+        }
+      )
+    }
+ }]);
+
+(function() {
+  "use strict";
+
+  app.factory("WardServices", ["$resource", function($resource) {
+    return $resource("https://api.github.com/repos/:username/:repo/issues", {
+      state: "open"
+    }, {
+      query: {
+        method: "GET",
+        isArray: true
+      }
+    });
+  }]);
+})();
