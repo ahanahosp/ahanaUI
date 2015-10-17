34762978ea2ba405a7c34d67a2b16edd37121a07
Selvaraj K <ahanahosp@gmail.com>
2015-10-17 20:39:57 +0530

    Fix

--- src/js/controllers/registration.js
+++ src/js/controllers/registration.js
@@ -7,49 +7,12 @@ app
 // Registration controller
     .controller('RegistrationController', ['$scope', '$http', function($scope, $http) {
 
-      $scope.today = function() {
-        $scope.dt = new Date();
-      };
-      $scope.today();
-
-      $scope.clear = function() {
-        $scope.dt = null;
-      };
-
-      // Disable weekend selection
-      $scope.disabled = function(date, mode) {
-        return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
-      };
-
-      $scope.toggleMin = function() {
-        $scope.minDate = $scope.minDate ? null : new Date();
-      };
-      $scope.toggleMin();
-
       $scope.open = function($event) {
         $event.preventDefault();
         $event.stopPropagation();
 
         $scope.opened = true;
       };
-
-      $scope.openDateOfBirth = function($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-
-        $scope.openedDateOfBirth = true;
-      };
-
-      $scope.dateOptions = {
-        formatYear: 'yy',
-        startingDay: 1,
-        class: 'datepicker'
-      };
-
-      $scope.initDate = new Date('2016-15-20');
-      $scope.formats = ['dd/MM/yyyy hh:mm:ss', 'dd/MM/yyyy'];
-      $scope.format = $scope.formats[0];
-      $scope.formatDOB = $scope.formats[1];
 
       $http.get(path + "rest/secure/lookup/loadLookupByName?lookupNames=country").then(
           function(response) {
@@ -80,7 +43,6 @@ app
             url: path + "rest/secure/registration/savePatient",
             method: "POST",
             data: $scope.data
-            //headers: {'Content-Type': 'application/x-www-form-urlencoded'}
           }).then(
               function() {
                 $scope.data = {};
