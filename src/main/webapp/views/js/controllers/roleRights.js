e74751ad7e1dff13e3d4ae72b33731518c74cc58
Hari <hari@arkinfotec.com>
2015-11-25 00:25:09 +0530

    Code updates

--- src/main/webapp/views/js/controllers/roleRights.js
+++ src/main/webapp/views/js/controllers/roleRights.js
@@ -1,3 +1,33 @@
+/**
+ * Searches an array for an object with a specified property.
+ *
+ * Example usage:
+ * <code>var index = myArray.indexOfObjectWithProperty('id', 123);</code>
+ *
+ * @param propertyName the name of the property to inspect
+ * @param propertyValue the value to match
+ * @returns the (zero-based) index at which the object was found, or -1 if no
+ * such object was found
+ */
+Array.prototype.indexOfObjectWithProperty = function (propertyName, propertyValue){
+  for (var i = 0, len = this.length; i < len; i++){
+    if (this[i][propertyName] === propertyValue) return i;
+  }
+  return -1;
+};
+/**
+ * Test if an array of objects contains an object with a specified property.
+ *
+ * Example usage:
+ * <code>var isPresent = myArray.containsObjectWithProperty('id', 123);</code>
+ *
+ * @param propertyName the name of the property to inspect
+ * @param propertyValue the value to match
+ * @returns true if an object was found, false otherwise
+ */
+Array.prototype.containsObjectWithProperty = function (propertyName, propertyValue){
+  return this.indexOfObjectWithProperty (propertyName, propertyValue) != -1;
+};
 'use strict';
 /* Controllers */
 app.controller ('RolesRightsController', ['$scope', '$http', 'NgTableParams', '$filter', '$state', 'modalService', '$rootScope', '$timeout', function ($scope, $http, NgTableParams, $filter, $state, modalService, $rootScope, $timeout){
@@ -28,13 +58,17 @@ app.controller ('RolesRightsController', ['$scope', '$http', 'NgTableParams', '$
     }
   );
   $scope.getSavedRights = function (){
-    $http.get (path + "rest/secure/user/getSavedRightsByRoleOid?roleOid="+ $scope.data.roleOid).then (
+    $http.get (path + "rest/secure/user/getSavedRightsByRoleOid?roleOid=" + $scope.data.roleOid).then (
       function (response){
-        $scope.modules = response.data.rightsDetails;
+        if (response.data.Status === 'Ok'){
+          $scope.selectedModules = response.data.rightsDetails;
+        }
+        else{
+          $scope.selectedModules = []
+        }
       }
     )
   }
-  
   $http ({
     url: path + "rest/secure/common/getDefaultOrganization",
     method: "GET"
@@ -48,33 +82,44 @@ app.controller ('RolesRightsController', ['$scope', '$http', 'NgTableParams', '$
         $scope.organizationErrorData = response.data;
       }
     }
-  )
+  );
+  $scope.selectedModules = [];
+  $scope.toggleSelection = function toggleSelection (module){
+    var index = $scope.selectedModules.indexOfObjectWithProperty ('oid', module.oid);
+    if (index > -1){
+      // Is currently selected, so remove it
+      $scope.selectedModules.splice (index, 1);
+    }
+    else{
+      // Is currently unselected, so add it
+      $scope.selectedModules.push (module);
+    }
+  };
   $scope.saveRoleRights = function (){
     $scope.errorData = "";
     $scope.successMessage = "";
-    var selectedRoles = [];
-    angular.forEach ($scope.data.modules, function (value, key){
-        angular.forEach (value, function (invalue, inkey){
-          if (invalue === 'ACT'){
-            selectedRoles.push (inkey);
-          }
-        });
+    var selectedRoleModules = [];
+    angular.forEach ($scope.selectedModules, function (value, key){
+        selectedRoleModules.push (value.oid);
       }
     );
     var data = {
       'roleOid': $scope.data.roleOid,
       'organizationOid': $scope.data.organizationOid,
-      'moduleOids': selectedRoles
+      'moduleOids': selectedRoleModules.join (",")
     };
     if ($scope.roleRightsForm.$valid){
       $http ({
-        url: path + "/rest/secure/user/saveRoleRights",
+        url: path + "rest/secure/user/saveRoleRights",
         method: "POST",
         data: data
       }).then (
         function (response){
           if (response.data.Status === 'Ok'){
-            $scope.data = {};
+            $scope.successMessage = "Role rights saved successfully";
+            $timeout (function (){
+              $state.reload ();
+            }, 1000);
           }
           else{
             $scope.errorData = response.data;
